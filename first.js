a41b85914ed82d64a4e83399c166d8afc04f5640
Amogh <amogh.jonnalagadda@gmail.com>
2021-02-09 15:35:50 +0800

    first commit

---
+++ first.js
@@ -0,0 +1,108 @@
+version
+
+>Es> Esma Script
+Javascript
+TypeScript
+
+Es5 > old
+Es6 > new
+
+//////////////////
+dataType
+>String
+>Number
+>Boolean
+>null
+>undefined
+//Es5//
+var a ="hi"
+var b = 10
+var c = true
+a
+"hi"
+b
+10
+c
+true
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "10"
+typeof(d)
+"string"
+var e = 10.998
+typeof (e)
+"number"
+
+var a = 10
+var b = 5
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+a%b
+
+10%5
+0
+4%3
+1
+4%2
+0
+
+modules of smalles to larger its always smaller
+
+var a = "Hi "
+var b = "Javascript"
+
+a+b
+"Hi Javascript"
+
+if we use + with number they will add
+if we use + with string they will concat
+
+var a = "Hi "
+var b = "Javascript"
+undefined
+a+b
+"Hi Javascript"
+var c = 10
+undefined
+a+b+c
+"Hi Javascript10"
+c+a
+"10Hi "
+a-b
+NaN > Not a number
+a*b
+NaN
+a/b
+NaN
+
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+"10"+10+"10"
+"1010"
+"101010"
+10+10+"10"
+"2010"
+"10"+10+"10"
+10+10+"10"
+"10"+10+10
+
+"10"+10+"10"-1
+101009
+10+10+"10"-1
+2009
+"10"+10+10-1
+101009
